c5afb9b224c284904fca04bcd9f3b82654e2fcab
Leo Alkberg <leo.alkberg@gmail.com>
2020-01-06 14:47:47 +0100

    fixed unity build

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public int PlayerNumber;
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (PlayerNumber)
+        {
+            case 1:
+                {
+                    if (Input.GetAxis("P1 Jump") > 0.1f)
+                    {
+                        Jump();
+                    }
+                    break;
+                }
+            case 2:
+                {
+                    if (Input.GetAxis("P2 Jump") > 0.1f)
+                    {
+                        Jump();
+                    }
+                    break;
+                }
+            case 3:
+                {
+                    if (Input.GetAxis("P3 Jump") > 0.1f)
+                    {
+                        Jump();
+                    }
+                    break;
+                }
+            case 4:
+                {
+                    if (Input.GetAxis("P4 Jump") > 0.1f)
+                    {
+                        Jump();
+                    }
+                    break;
+                }
+        }
+    }
+
+    void Jump()
+    {
+        rb.AddForce(new Vector2(0, 10));
+    }
+}
---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    [Range(2, 4)] public int NumberOfPlayers = 2; //contains the number of players that will be controlled by how many controllers. NEVER less than 4
+
+    public GameObject PlayerControllerPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 1; i <= NumberOfPlayers; i++)
+        {
+            GameObject controller = Instantiate(PlayerControllerPrefab);
+            PlayerController controllerComponent = controller.GetComponent<PlayerController>();
+            controllerComponent.PlayerNumber = i;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
